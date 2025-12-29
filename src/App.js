3814e8abdcd613ea320f499c5223afd7f7d8610b
Triloki <trilokisaini61@gmail.com>
2025-12-29 20:00:29 +0530

    Add text

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import { BrowserRouter as Router, Route,  Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import SearchResult from './pages/SearchResult';
 import RecipeDetails from './pages/RecipeDetails';
 
 function App() {
-
- 
-return (
-  < >
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/result/:inputValue' element={<SearchResult/>} />
-        <Route path='/recipe/:id' element={<RecipeDetails/>}/>
-      </Routes>
-    </Router>
-  </>
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/result/:inputValue' element={<SearchResult />} />
+          <Route path='/recipe/:id' element={<RecipeDetails />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
